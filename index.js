6edc18b757c1afc87d6817bb73a6efe7fa396091
Elvis Kuria <levonjoroge>
2024-10-01 20:31:04 +0300

    created index.js

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 62;
+let num2 = 1;
+let multiply = num1 * num2;
+console.log(multiply);
 
-// Write your code here
+let random = Math.floor(Math.random()*7);
+console.log(random)
+
+let num3 = 24
+let num4 = 20
+let mod = num3 % num4;
+console.log(mod);
+
+let numbers = (0,5,10,15,20)
+let max = Math.max(0,5,10,15,20)
